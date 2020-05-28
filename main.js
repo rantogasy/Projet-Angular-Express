1a81a31c3eace599d8ae1107bd16e153a6b2a22d
rantogasy <50084062+rantogasy@users.noreply.github.com>
2020-05-29 00:00:04 +0200

    modif d'ajout d'article

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const mysql = require('mysql')
+const http = require('http')
 const path = require('path')
 const cors = require('cors')
 
@@ -22,7 +23,7 @@ db.connect(function(err) {
 })
 
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 
@@ -49,33 +50,30 @@ app.get('/:categorie', (request, response) => {
     db.query("SELECT nomArticle, descriptionArticle, dateArticle FROM article", function (err, result, fields)  {
       if (err) throw err;
       console.log(result);
-      response.json(result)
+      response.end(JSON.stringify(result))
     })
   })
-
-
 
 
 //Retourne un article au format JSON
   app.get('/list_article/:nomArticle', (request, response) => {
     nomArticle = request.params.nomArticle
-    db.query("SELECT nomArticle, descriptionArticle, dateArticle FROM article WHERE nomArticle = ?",[nomArticle] , function (err, result, fields)  {
+    db.query("SELECT nomArticle, descriptionArticle, dateArticle FROM article WHERE nomArticle = ?", [nomArticle], function (err, result, fields)  {
       if (err) throw err;
       console.log(result);
-      response.json(result)
+      response.end(JSON.stringify(result))
     })
   })
 
 
 //Insertion d'un article via le formulaire
-  app.post('/save_article/', (request, response) => {
+  app.post('/save_article', (request, response) => {
     data = request.body
-    var sql = `INSERT INTO article (idArticle, nomArticle, descriptionArticle, dateArticle) VALUES('', '${data.nomArticle}', '${data.descriptionArticle}, '')`
-    conn.query(sql, function (err, result) {
+    db.query('INSERT INTO article SET ?', data, function (err, result) {
       if (err) throw err
       console.log(`A row has been inserted!`)
       console.log(result)
-      response.json(result)
+      response.end(JSON.stringify(result))
     })
   })
 
@@ -83,4 +81,12 @@ app.get('/:categorie', (request, response) => {
 
 
 
-app.listen(8080, () => console.log('Listening on port 8080'))
+var server = app.listen(8080, "localhost", function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("App currently listening at http://%s:%s", host, port)
+
+})
+//app.listen(8080, () => console.log('Listening on port 8080'))
