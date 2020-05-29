3a01d4423f6d1f8ac448c5a2dd9bd6922f4b075e
rantogasy <50084062+rantogasy@users.noreply.github.com>
2020-05-30 00:04:21 +0200

    ajout du signin

--- user.js
+++ user.js
@@ -23,6 +23,70 @@ exports.signup = function(request , response){
   })
 }
 
-
+exports.signin = function(request, response) {
+	var email =request.body.email
+ 	var password = request.body.password
+ 	var dec_password = atob(password)
+	var encrypted_password = cryptr.encrypt(dec_password)
+	
+	 var sql="SELECT idLogin, first_name, last_name, email FROM login WHERE email = ? and password = ?"
+	 
+	 db.query(sql, [email, password], function(err, result) {	
+		 
+		 if(result != ""){
+			 
+			 console.log(JSON.stringify(result))
+			 
+			 var data = JSON.stringify(result)
+			 
+			 var secret = 'TOPSECRETTTTT'
+				var now = Math.floor(Date.now() / 1000),
+					iat = (now - 10),
+					expiresIn = 3600,
+					expr = (now + expiresIn),
+					notBefore = (now - 10),
+					jwtId = Math.random().toString(36).substring(7);
+				var payload = {
+					iat: iat,
+					jwtid : jwtId,
+					audience : 'TEST',
+					data : data
+				}	
+				
+			 
+			 jwt.sign(payload, secret, { algorithm: 'HS256', expiresIn : expiresIn}, function(err, token) {
+					
+				if(err){
+					console.log('Error occurred while generating token')
+					console.log(err)
+					return false
+				}
+				 else{
+				if(token != false){
+					//res.send(token);
+					response.header()
+					response.json({
+  						"results":
+  								{"status": "true"},
+  						"token" : token,
+						"data" : result
+										
+  					});
+					response.end(JSON.stringify(result))
+				}
+				else{
+					response.send("Could not create token")
+					response.end()
+				}
+				
+				 }
+			})
+		
+		 }
+		 else if(result == ""){
+			 	console.log("not a user")
+		 }
+	 })
+}
 
 //{"idLogin":"", "first_name":"ranto", "lastname":"ralijaona", "email":"mdr", "password":"password"}
