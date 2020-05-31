b16dc0a8cf4800c3bcf64c23601dcffd873e96a3
rantogasy <50084062+rantogasy@users.noreply.github.com>
2020-05-31 23:19:31 +0200

    ajout de commentaires

--- src/JS/user.js
+++ src/JS/user.js
@@ -3,8 +3,9 @@ const db = require('./bdd.js')
 const atob = require('atob')
 const Cryptr = require('cryptr')
 cryptr = new Cryptr('myTotalySecretKey')
- 
 
+ 
+//inscription d'un utilisateur
 exports.signup = function(request , response){
    var data = request.body
    var first_name = request.body.first_name
@@ -23,13 +24,14 @@ exports.signup = function(request , response){
   })
 }
 
+//connexion d'un utilisateur avec un JWT
 exports.signin = function(request, response) {
 	var email =request.body.email
  	var password = request.body.password
  	var dec_password = atob(password)
 	var encrypted_password = cryptr.encrypt(dec_password)
 	
-	 var sql="SELECT idLogin, first_name, last_name, email FROM login WHERE email = ? and password = ?"
+	var sql="SELECT idLogin, first_name, last_name, email FROM login WHERE email = ? and password = ?"
 	 
 	 db.query(sql, [email, password], function(err, result) {	
 		 
@@ -71,7 +73,8 @@ exports.signin = function(request, response) {
   						"token" : token,
 						"data" : result
 										
-  					});
+  					})
+  					console.log(token)
 					response.end(JSON.stringify(result))
 				}
 				else{
